6342eb46add63aab12cdceed6603ca287e382c6e
Yusuf4ic <yusufsayfullaev12@gmail.com>
2025-01-02 21:53:20 +0500

    true

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Text('GGG'),
+        child: Text('Ysuududd'),
       ),
     );
   }
