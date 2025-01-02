b379b272cd2fe15df0e1b7ec7124504dd4c435ca
Yusuf4ic <yusufsayfullaev12@gmail.com>
2025-01-02 22:50:44 +0500

    true

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,9 +5,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text('Ysuududd'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.indigo,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 350,
+                  child: Image.asset('assets/image/rain.png')),
+                Text(
+                  'Weather',
+                  style: TextStyle(
+                      fontSize: 44,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+                Text(
+                  'ForeCasts',
+                  style: TextStyle(
+                    fontSize: 44,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromRGBO(221, 177, 48, 1),
+                  ),
+                ),
+                SizedBox(
+                  width: 204,
+                  height: 52,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(221, 177, 48, 1)),
+                    child: Text('Get Start'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
